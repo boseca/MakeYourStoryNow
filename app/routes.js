523005d6791dbe1240ffc3a8f4608b4fecac5b32
Bose <bose_nc@hotmail.com>
2017-10-02 05:30:28 -0400

    First draft (reverted from commit dc7b67a3b0d9e93253e984eae96fc00ca13b163a)

--- app/routes.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = function(app) {
-    app.get('*', function(req, res) {
-        // res.sendFile('../public/index.html', { root:  __dirname});
-        res.sendFile(path.resolve('front-end/app/index.html'));
-
-    });
-};
