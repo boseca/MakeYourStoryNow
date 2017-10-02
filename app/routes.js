dc7b67a3b0d9e93253e984eae96fc00ca13b163a
Bose <bose_nc@hotmail.com>
2017-10-02 05:18:58 -0400

    First draft

---
+++ app/routes.js
@@ -0,0 +1,9 @@
+var path = require('path');
+
+module.exports = function(app) {
+    app.get('*', function(req, res) {
+        // res.sendFile('../public/index.html', { root:  __dirname});
+        res.sendFile(path.resolve('front-end/app/index.html'));
+
+    });
+};
---
+++ app/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+//   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+//     next();
+// });
+
+//New lines!
+app.get('/api/',function(req,res) {
+	res.send('Working');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+require('./routes')(app);
+
+app.listen(port);
+
+exports = module.exports = app;
